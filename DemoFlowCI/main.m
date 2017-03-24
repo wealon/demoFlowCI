51f0da0783c55f7c896427be3f724ab969a2aa54
wealon <linxianliuzhixiang@163.com>
2017-03-24 13:42:23 +0800

    aaa

---
+++ DemoFlowCI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DemoFlowCI
+//
+//  Created by wealon on 2017/3/21.
+//  Copyright © 2017年 MDJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
